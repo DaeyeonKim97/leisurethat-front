d283eabac6f31fb1d2d4fc21f65be8bc25411477
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-15 15:11:11 +0900

    feature:admin: JudgePage생성

--- src/admin/pages/JudgePage.js
+++ src/admin/pages/JudgePage.js
@@ -1,6 +1,14 @@
+import Divider from '@mui/material/Divider';
 
 export default function JudgePage(){
     return(
-        <h1>등록심사페이지</h1>
+        <section style={{margin : 40}}>
+            <header 
+                style={{margin : "15px 0px",color : "#00AEEF", fontSize:48 , fontWeight:"bold"}}
+            >
+                프로젝트 등록 심사
+            </header>
+            <Divider style={{boxShadow: "0px 3px 6px gray"}}/>
+        </section>
     )
 }
