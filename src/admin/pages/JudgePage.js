253c83c1469d579e118b1f05dd3883f6137e91a8
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-15 22:13:49 +0900

    feature:admin: JudgePage 검색 폼 추가

--- src/admin/pages/JudgePage.js
+++ src/admin/pages/JudgePage.js
@@ -1,14 +1,18 @@
 import Divider from '@mui/material/Divider';
+import JudgeSearchForm from '../components/judge/JudgeSearchForm';
 
 export default function JudgePage(){
     return(
         <section style={{margin : 40}}>
             <header 
-                style={{margin : "15px 0px",color : "#00AEEF", fontSize:48 , fontWeight:"bold"}}
+                style={{color : "#00AEEF", fontSize:48 , fontWeight:"bold"}}
             >
                 프로젝트 등록 심사
             </header>
-            <Divider style={{boxShadow: "0px 3px 6px gray"}}/>
+
+            <Divider style={{boxShadow: "0px 2px 4px gray", margin : "15px 0px 25px 0px"}}/>
+
+            <JudgeSearchForm/>
         </section>
     )
 }
