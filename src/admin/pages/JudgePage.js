4c067001e797cc46f1ea59405d7a31ae1cd4921f
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-16 20:37:39 +0900

    feature:admin: JudgePage css 비율 조정

--- src/admin/pages/JudgePage.js
+++ src/admin/pages/JudgePage.js
@@ -4,7 +4,7 @@ import JudgeTable from '../components/judge/JudgeTable';
 
 export default function JudgePage(){
     return(
-        <section style={{margin : 40}}>
+        <section style={{padding : 40, height:'calc(100% - 64px)'}}>
             <header 
                 style={{color : "#00AEEF", fontSize:36 , fontWeight:"bold"}}
             >
@@ -12,13 +12,18 @@ export default function JudgePage(){
             </header>
 
             <Divider style={{boxShadow: "0px 2px 4px gray", margin : "15px 0px 25px 0px"}}/>
+
             <JudgeSearchForm/>
 
-            <section>
-                총 20 개
-            </section>
+            <div className="whatsthis" style={{display:'flex', justifyContent:'center', flexDirection:'column', height:"calc(100% - 120px)"}}>
 
-            <JudgeTable/>
+                <section style={{margin:'12px 20px', fontSize:'18px', alignSelf:'flex-end'}}>
+                    총 <span style={{color:'#6297BA'}}>22320</span> 개
+                </section>
+
+                <JudgeTable/>
+
+            </div>
         </section>
     )
 }
