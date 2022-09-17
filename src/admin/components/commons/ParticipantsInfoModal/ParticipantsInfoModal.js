8145ea6802fc76ea8453e17b504e72df3a38042b
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-17 23:25:06 +0900

    feature:admin: ParticipantsInfo 모달 prototype 작성

---
+++ src/admin/components/commons/ParticipantsInfoModal/ParticipantsInfoModal.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { Divider,Alert,AlertTitle } from '@mui/material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 1024,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  borderRadius: 2,
+  p: 4,
+};
+
+export default function ParticipantsInfoModal(props) {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <div onClick={handleOpen}>{props.children}</div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            프로젝트 상세정보
+          </Typography>
+          <Divider style={{marginBottom:'20px'}}/>
+            Modal test
+        </Box>
+      </Modal>
+    </div>
+  );
+}
