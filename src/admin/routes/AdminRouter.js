90d66446e58ef800122096e281d5c7bdbd29e4f9
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-18 19:37:42 +0900

    feature:admin: Test용 Router 수정

--- src/admin/routes/AdminRouter.js
+++ src/admin/routes/AdminRouter.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AdminLayout from '../layouts/AdminLayout'
 import JudgePage from '../pages/JudgePage'
+import PreOpenPage from '../pages/PreOpenPage'
 
 export default function AdminRouter() {
   console.log('admin router')
   return (
     <Routes>
       <Route path="/" element={<AdminLayout />}>
-        <Route path="*" element={<JudgePage />} />
+        <Route path="judge" element={<JudgePage />} />
+        <Route path="pre-open" element={<PreOpenPage />} />
       </Route>
     </Routes>
   )
