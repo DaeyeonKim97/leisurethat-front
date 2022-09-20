c06827351de86431288421d1d7c0a273538105d4
Junho Park <dwarfthema@gmail.com>
2022-09-20 16:18:42 +0900

    feat:public:Buying페이지 깡통완성

--- src/public/components/shared.js
+++ src/public/components/shared.js
@@ -17,3 +17,21 @@ export const UnderLineBox = styled.div`
   justify-content: space-between;
   align-items: flex-end;
 `
+
+export const UnderLineContentsBox = styled(UnderLineBox)`
+  font-size: 26px;
+  margin-bottom: 10px;
+  border-color: rgba(0, 0, 0, 1);
+  border-width: 2px;
+`
+
+export const UnderLineContentBox = styled(UnderLineBox)`
+  font-size: 18px;
+  padding: 10px;
+  justify-content: flex-start;
+`
+
+export const UnderLineContent = styled.div`
+  margin-right: 20px;
+  width: 100%;
+`
--- src/public/routes/PublicRouter.js
+++ src/public/routes/PublicRouter.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PublicLayout from '../layouts/PublicLayout'
 import PublicBuyComplete from '../pages/PublicBuyComplete'
+import PublicBuying from '../pages/PublicBuying'
 import PublicLogin from '../pages/PublicLogin'
 import PublicMain from '../pages/PublicMain'
 export default function PublicRouter() {
@@ -10,6 +11,7 @@ export default function PublicRouter() {
         <Route path="/" element={<PublicMain />} />
         <Route path="/Login" element={<PublicLogin />} />
         <Route path="/Complete" element={<PublicBuyComplete />} />
+        <Route path="/Buying" element={<PublicBuying />} />
       </Route>
     </Routes>
   )
