6bc8da3ec6ecc2b43ebf0c4fa67a94ae39e2b6f0
hyunjungjeon <sksznddl574@gmail.com>
2022-09-22 08:23:32 +0900

    feature/public/mypage 라우터 추가

--- src/public/routes/PublicRouter.js
+++ src/public/routes/PublicRouter.js
@@ -6,16 +6,19 @@ import PublicLogin from '../pages/PublicLogin'
 import PublicMain from '../pages/PublicMain'
 import PublicSignup from '../pages/PublicSignup'
 import MatchId from '../pages/MatchId'
+import Mypage from '../pages/Mypage'
+
 export default function PublicRouter() {
   return (
     <Routes>
       <Route path="/" element={<PublicLayout />}>
         <Route path="/" element={<PublicMain />} />
         <Route path="/Login" element={<PublicLogin />} />
-        <Route path="/signup" element={< PublicSignup/>} />
-        <Route path="/match/id" element={< MatchId/>} />
+        <Route path="/signup" element={<PublicSignup />} />
+        <Route path="/match/id" element={<MatchId />} />
         <Route path="/Complete" element={<PublicBuyComplete />} />
         <Route path="/Buying" element={<PublicBuying />} />
+        <Route path="mypage" element={<Mypage />} />
       </Route>
     </Routes>
   )
