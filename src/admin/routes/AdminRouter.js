a262ee82a20d9f0c10adc10e7b0f2ff5f226b3b2
hyunjungjeon <sksznddl574@gmail.com>
2022-09-21 09:13:46 +0900

    feature/admin/관리자 정산 페이지 제작

--- src/admin/routes/AdminRouter.js
+++ src/admin/routes/AdminRouter.js
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AdminLayout from '../layouts/AdminLayout'
-import JudgePage from '../pages/JudgePage'
-import PreOpenPage from '../pages/PreOpenPage'
-import ProceedingPage from '../pages/ProceedingPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CalculatePage from "../pages/CalculatePage";
+import AdminLayout from "../layouts/AdminLayout";
+import JudgePage from "../pages/JudgePage";
+import PreOpenPage from "../pages/PreOpenPage";
+import ProceedingPage from "../pages/ProceedingPage";
 
 export default function AdminRouter() {
-  console.log('admin router')
+  console.log("admin router");
   return (
     <Routes>
       <Route path="/" element={<AdminLayout />}>
         <Route path="judge" element={<JudgePage />} />
         <Route path="pre-open" element={<PreOpenPage />} />
         <Route path="proceeding" element={<ProceedingPage />} />
+        <Route path="calculate" element={<CalculatePage />} />
       </Route>
     </Routes>
-  )
+  );
 }
