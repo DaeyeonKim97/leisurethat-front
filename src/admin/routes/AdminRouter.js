3215dcaf545a705ad37f63a26dc2c9139ee9175d
Daeyeon Kim <64251365+DaeyeonKim97@users.noreply.github.com>
2022-09-21 09:06:35 +0900

    Feature/admin page/dykim97 (#12)
    * fix:admin: AdminDrawer 포기된 프로젝트 링크 변경
    
    * feature:admin: AdminRouter에 GiveupPage 연결
    
    * feature:admin: 포기승인모달 작성
    
    * feature:admin: GiveupPage pagenation 프로토타입 작성
    
    * feature:admin: GiveupTable 작성
    
    * feature:admin: 포기 사유 모달 작성
    
    * feature:admin: 포기승인 페이지 서치폼 작성
    
    * feature:admin: GiveupPage에 연결

--- src/admin/routes/AdminRouter.js
+++ src/admin/routes/AdminRouter.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AdminLayout from '../layouts/AdminLayout'
+import GiveupPage from '../pages/GiveupPage'
 import JudgePage from '../pages/JudgePage'
 import PreOpenPage from '../pages/PreOpenPage'
 import ProceedingPage from '../pages/ProceedingPage'
@@ -12,6 +13,7 @@ export default function AdminRouter() {
         <Route path="judge" element={<JudgePage />} />
         <Route path="pre-open" element={<PreOpenPage />} />
         <Route path="proceeding" element={<ProceedingPage />} />
+        <Route path="giveup" element={<GiveupPage />} />
       </Route>
     </Routes>
   )
