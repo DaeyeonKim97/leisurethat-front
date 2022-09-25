86741c0d36cf23121ba5497823805988d175421e
hyunjungjeon <sksznddl574@gmail.com>
2022-09-26 01:28:36 +0900

    fix/public/mypage 제작한 프로젝트 상태별 추가

--- src/public/components/mypage/MakeProjectItemGrid.js
+++ src/public/components/mypage/MakeProjectItemGrid.js
@@ -1,21 +1,164 @@
-import MakeProjectItem from './MakeProjectItem';
-import Grid from '@mui/material/Unstable_Grid2';
+import MakeProjectItem from "./MakeProjectItem";
+import Grid from "@mui/material/Unstable_Grid2";
 
 function MakeProjectItemGrid() {
+  function createData(
+    projectId,
+    projectName,
+    category,
+    projectState,
+    businessName,
+    achievements,
+    targetAmount
+  ) {
+    return {
+      projectId,
+      projectName,
+      category,
+      projectState,
+      businessName,
+      achievements,
+      targetAmount,
+    };
+  }
+
+  const datas = [
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "심사요청",
+      "사업자명",
+      0,
+      "30,000,000원"
+    ),
+    createData("1", "에스튜브", "캠핑", "반려", "사업자명", 0, "30,000,000원"),
+    createData("1", "에스튜브", "캠핑", "취소", "사업자명", 0, "30,000,000원"),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "펀딩포기",
+      "사업자명",
+      0,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "강제종료",
+      "사업자명",
+      20,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "오픈예정",
+      "사업자명",
+      0,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "펀딩 진행중",
+      "사업자명",
+      40,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "미달성종료",
+      "사업자명",
+      70,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "진행여부결정",
+      "사업자명",
+      90,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "환불진행",
+      "사업자명",
+      120,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "결제 후 종료",
+      "사업자명",
+      120,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "정산 대기",
+      "사업자명",
+      100,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "1차 정산 요청",
+      "사업자명",
+      100,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "1차 정산 완료",
+      "사업자명",
+      100,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "2차 정산 요청",
+      "사업자명",
+      100,
+      "30,000,000원"
+    ),
+    createData(
+      "1",
+      "에스튜브",
+      "캠핑",
+      "완료",
+      "사업자명",
+      100,
+      "30,000,000원"
+    ),
+  ];
   return (
     <Grid container rowSpacing={5} columnSpacing={{ xs: 6, sm: 2, md: 3 }}>
-      <Grid xs={5}>
-        <MakeProjectItem />
-      </Grid>
-      <Grid xs={5}>
-        <MakeProjectItem />
-      </Grid>
-      <Grid xs={5}>
-        <MakeProjectItem />
-      </Grid>
-      <Grid xs={5}>
-        <MakeProjectItem />
-      </Grid>
+      {datas.map((data, id) => (
+        <Grid xs={5} key="">
+          <MakeProjectItem data={data} />
+        </Grid>
+      ))}
     </Grid>
   );
 }
