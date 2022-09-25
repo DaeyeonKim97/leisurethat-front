48f98290c88b5126e8df235dcde6bda03957c00c
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-25 20:07:14 +0900

    feature:public: 프로젝트Detail-Qna 탭메뉴 구현

---
+++ src/public/components/projectDetail/qna/DetailQnaTable.js
@@ -0,0 +1,146 @@
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import { Typography, Button } from '@mui/material'
+import DetailQnaPagenation from './DetailQnaPagenation'
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#6297BA',
+    color: theme.palette.common.white,
+    fontWeight: 'bold',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}))
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(even)': {
+    backgroundColor: '#E6F4F150',
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}))
+
+function createData(id, name, author, created) {
+  return {
+    id,
+    name,
+    author,
+    created,
+  }
+}
+
+const rows = [
+  createData(
+    1,
+    '프로젝트 문의사항입니다 어쩌구 저쩌구',
+    'leisurethat',
+    '2022-01-01'
+  ),
+  createData(
+    2,
+    '프로젝트 문의사항입니다 어쩌구 저쩌구',
+    'leisu23',
+    '2022-01-02'
+  ),
+  createData(
+    3,
+    '프로젝트 문의사항입니다 어쩌구 저쩌구',
+    'l2314ethat',
+    '2022-01-03'
+  ),
+  createData(
+    4,
+    '프로젝트 문의사항입니다 어쩌구 저쩌구',
+    'leis231that',
+    '2022-01-04'
+  ),
+  createData(
+    5,
+    '프로젝트 문의사항입니다 어쩌구 저쩌구',
+    'le421that',
+    '2022-01-05'
+  ),
+  createData(
+    6,
+    '프로젝트 문의사항입니다 어쩌구 저쩌구',
+    'lei412that',
+    '2022-01-06'
+  ),
+]
+
+export default function DetailQnaTable() {
+  return (
+    <>
+      <TableContainer
+        component={Paper}
+        sx={{ height: '95%' }}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Typography
+          height="40px"
+          variant="h6"
+          id="tableTitle"
+          component="div"
+          padding="5px 15px"
+          backgroundColor="#6297BAA0"
+          color="white"
+        >
+          전체 결과
+        </Typography>
+        <Table
+          sx={{ minWidth: 700, height: '0.8' }}
+          style={{ flex: 1 }}
+          aria-label="customized table"
+        >
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center" sx={{ width: 130 }}>
+                번호
+              </StyledTableCell>
+              <StyledTableCell align="center">제목</StyledTableCell>
+              <StyledTableCell align="center">작성자</StyledTableCell>
+              <StyledTableCell align="center">작성일</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.id} hover>
+                <StyledTableCell component="th" scope="row" sx={{ width: 100 }}>
+                  {row.id}
+                </StyledTableCell>
+                <StyledTableCell align="center">{row.name}</StyledTableCell>
+                <StyledTableCell align="center">{row.author}</StyledTableCell>
+                <StyledTableCell align="center">{row.created}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '10px',
+        }}
+      >
+        <DetailQnaPagenation />
+      </div>
+    </>
+  )
+}
