5749d9a7246649cbba528fe69080b24f2ed05b67
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-25 12:51:23 +0900

    feature:public: 프로젝트 디테일 페이지 라우터 연결

--- src/public/routes/PublicRouter.js
+++ src/public/routes/PublicRouter.js
@@ -6,16 +6,18 @@ import PublicLogin from '../pages/PublicLogin'
 import PublicMain from '../pages/PublicMain'
 import PublicSignup from '../pages/PublicSignup'
 import MatchId from '../pages/MatchId'
+import ProjectDetail from '../pages/ProjectDetail'
 export default function PublicRouter() {
   return (
     <Routes>
       <Route path="/" element={<PublicLayout />}>
         <Route path="/" element={<PublicMain />} />
         <Route path="/Login" element={<PublicLogin />} />
-        <Route path="/signup" element={< PublicSignup/>} />
-        <Route path="/match/id" element={< MatchId/>} />
+        <Route path="/signup" element={<PublicSignup />} />
+        <Route path="/match/id" element={<MatchId />} />
         <Route path="/Complete" element={<PublicBuyComplete />} />
         <Route path="/Buying" element={<PublicBuying />} />
+        <Route path="/project-detail/:projectId" element={<ProjectDetail />} />
       </Route>
     </Routes>
   )
