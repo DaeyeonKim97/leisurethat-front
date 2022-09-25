b7ddf3d458dddb272b1701b9819bc3d76f957254
hyunjungjeon <sksznddl574@gmail.com>
2022-09-26 01:29:18 +0900

    feature/public/mypage 제작한 프로젝트 사유 모달 추가(상세X)

---
+++ src/public/components/mypage/ProjectJudgeForm.js
@@ -0,0 +1,7 @@
+export default function ProjectJudgeFrom() {
+  return (
+    <>
+      <div>프로젝트 심사 폼</div>
+    </>
+  );
+}
---
+++ src/public/components/mypage/ReasonForm.js
@@ -0,0 +1,7 @@
+export default function ReasonForm() {
+  return (
+    <>
+      <div>사유 확인 폼</div>
+    </>
+  );
+}
