d49b7ed5a75dd28777bb37756e11d97859c15099
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-13 09:42:32 +0900

    feature:admin: proceeding page api 연결

--- src/admin/components/proceeding/ProceedingTable.js
+++ src/admin/components/proceeding/ProceedingTable.js
@@ -59,7 +59,7 @@ const rows = [
   createData(6, '프로젝트 명', 'leisurethat01', '2022-01-01', 15, 'Y'),
 ]
 
-export default function ProceedingTable() {
+export default function ProceedingTable({ list }) {
   return (
     <>
       <TableContainer
@@ -100,15 +100,15 @@ export default function ProceedingTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <StyledTableRow key={row.id} hover>
+            {list.map((row) => (
+              <StyledTableRow key={row.projectId} hover>
                 <StyledTableCell
                   component="th"
                   scope="row"
                   sx={{ width: 100 }}
                   align="center"
                 >
-                  {row.id}
+                  {row.projectId}
                 </StyledTableCell>
                 <StyledTableCell align="center">
                   <div
@@ -119,7 +119,7 @@ export default function ProceedingTable() {
                       justifyContent: 'center',
                     }}
                   >
-                    {row.name}
+                    {row.projectName}
                     <ProjectInfoModal>
                       <IconButton
                         color="primary"
@@ -139,7 +139,7 @@ export default function ProceedingTable() {
                       justifyContent: 'center',
                     }}
                   >
-                    {row.makerID}
+                    {row.makerName}
                     <MakerInfoModal>
                       <IconButton
                         color="primary"
@@ -150,7 +150,7 @@ export default function ProceedingTable() {
                     </MakerInfoModal>
                   </div>
                 </StyledTableCell>
-                <StyledTableCell align="center">{row.end}</StyledTableCell>
+                <StyledTableCell align="center">{row.endDate}</StyledTableCell>
                 <StyledTableCell align="center">
                   <ParticipantsInfoModal>
                     <Button
@@ -158,7 +158,7 @@ export default function ProceedingTable() {
                       style={{ margin: '0px 10px' }}
                       size="midium"
                     >
-                      총 {row.participant} 명
+                      총 {row.participantNum} 명
                     </Button>
                   </ParticipantsInfoModal>
                 </StyledTableCell>
@@ -172,9 +172,9 @@ export default function ProceedingTable() {
                       marginLeft: '30%',
                     }}
                   >
-                    {row.status === 'Y' ? (
+                    {row.projectStatus === '모집중' ? (
                       <>
-                        펀딩 진행중
+                        모집중
                         <ProceedingRefuseModal>
                           <IconButton color="primary">
                             <HighlightOffIcon color="primary" />
--- src/admin/pages/ProceedingPage.js
+++ src/admin/pages/ProceedingPage.js
@@ -1,8 +1,37 @@
 import Divider from '@mui/material/Divider'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import ProceedingSearchForm from '../components/proceeding/ProceedingSearchForm'
 import ProceedingTable from '../components/proceeding/ProceedingTable'
 
 export default function ProceedingPage() {
+  const [init, setInit] = useState(true)
+  const [list, setList] = useState([])
+  const [count, setCount] = useState(0)
+  const [page, setPage] = useState(0)
+  const size = 6
+
+  useEffect(() => {
+    if (init) {
+      axios
+        .get(
+          `http://localhost:8001/project-detail/open?page=${page}&size=${size}`,
+          {
+            headers: {
+              Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data)
+          setList(res.data.results.projectList)
+          setCount(res.data.results.total)
+          console.log('list : ', list)
+        })
+
+      setInit(false)
+    }
+  }, [])
   return (
     <section style={{ padding: 40, height: 'calc(100% - 64px)' }}>
       <header style={{ color: '#00AEEF', fontSize: 36, fontWeight: 'bold' }}>
@@ -31,10 +60,10 @@ export default function ProceedingPage() {
             alignSelf: 'flex-end',
           }}
         >
-          총 <span style={{ color: '#6297BA' }}>22320</span> 개
+          총 <span style={{ color: '#6297BA' }}>{count}</span> 개
         </section>
 
-        <ProceedingTable />
+        <ProceedingTable list={list} />
       </div>
     </section>
   )
