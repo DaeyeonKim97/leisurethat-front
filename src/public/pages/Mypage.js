1a46dab6dbc5431be2e9b2262a19ed6646fb0665
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-13 21:00:23 +0900

    hotfix: merge bug fix 1차

--- src/public/pages/Mypage.js
+++ src/public/pages/Mypage.js
@@ -1,37 +1,37 @@
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
-import styled from "styled-components";
+import styled from 'styled-components'
 
-import MypageTab from "../components/mypage/MypageTab";
+import MypageTab from '../components/mypage/MypageTab'
 import {
   callGetMyFundingList,
   callGetMyProjectList,
-} from "../apis/MypageAPICalls";
+} from '../apis/MyPageAPICalls'
 
 function Mypage() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(callGetMyFundingList());
-    dispatch(callGetMyProjectList());
-  });
+    dispatch(callGetMyFundingList())
+    dispatch(callGetMyProjectList())
+  })
   const Title = styled.div`
     text-align: center;
     padding: 2%;
     border-bottom: 1px solid #d9d9d9; ;
-  `;
+  `
 
   return (
     <>
       <Title>
-        <h1 style={{ fontSize: "20px", fontWeight: "bold" }} align="center">
+        <h1 style={{ fontSize: '20px', fontWeight: 'bold' }} align="center">
           마이페이지
         </h1>
       </Title>
       <MypageTab />
     </>
-  );
+  )
 }
 
-export default Mypage;
+export default Mypage
